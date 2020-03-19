6295c8aaab6d1790fa2936b6bb4f1f2c180f52ef
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2020-03-19 14:15:56 +0530

    adjusted codebase for deployment

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 from flask import jsonify
-from services.get_data import get_data
-from services.get_helpline_numbers import helpline_numbers
+from .services.get_data import get_data
+from .services.get_helpline_numbers import helpline_numbers
 
 app = Flask(__name__)
 CORS(app)
@@ -26,6 +26,3 @@ def helplines():
         'disclaimer': "Please refer this link for updated helpline numbers https://www.mohfw.gov.in/pdf/coronvavirushelplinenumber.pdf. This data is provided to the public strictly for educational and academic research purposes. The API relies upon publicly available data from multiple sources, that do not always agree. We hereby disclaim any and all representations and warranties with respect to the API, including accuracy, fitness for use, and merchantability. Reliance on the API for medical guidance or use of the API in commerce is strictly prohibited.",
         'central_helpline': "+91-11-23978046"
     })
-
-if __name__ == '__main__':
-    app.run()
